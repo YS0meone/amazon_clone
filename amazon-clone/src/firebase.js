f426864b879d4b2a378f3db2e5f955c1af24e0b0
YS0meone <timyuan1016@gmail.com>
2023-08-28 18:48:30 -0700

    Remove unnecessary files from the framework

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBb1Ad3YTpHzagSdrHvlHQJUg--WXtkE7E",
+    authDomain: "clone-83ab7.firebaseapp.com",
+    projectId: "clone-83ab7",
+    storageBucket: "clone-83ab7.appspot.com",
+    messagingSenderId: "467593657303",
+    appId: "1:467593657303:web:73e0ce6a3dcbe0a00b2992",
+    measurementId: "G-92LTN4GE3G"
+  };
