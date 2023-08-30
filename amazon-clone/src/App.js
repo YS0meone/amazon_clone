1f84f18b07133fd7b800bf2d04f86ae0be19ff0b
YS0meone <timyuan1016@gmail.com>
2023-08-29 17:58:57 -0700

    Build basic header section

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,9 +1,14 @@
+import React from "react";
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello World!</h1> 
+      <h1>Hello World!</h1>
+      {/* Header */}
+      {/* Home */}
+      <Header />
     </div>
   );
 }
---
+++ amazon-clone/src/Header.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <div className="header">
+            {/* LOGO */}
+            <img className='header__logo' src='logo.png' />
+            {/* Search Bar */}
+            <div className="header__search">
+                <input className='heade__searchInput' type="text" />
+            </div>
+            {/* Header */}
+            <div className='header__nav'>
+                <div className='header__option'>
+                    <span className='header__optionLineOne'>Hello Guest</span>
+                    <span className='header__optionLineOne'>Sign In</span>
+                </div>
+                <div className='header__option'>
+                    <span className='header__optionLineOne'>Returns</span>
+                    <span className='header__optionLineOne'>& Orders</span>
+                </div>
+                <div className='header__option'>
+                    <span className='header__optionLineOne'>Your</span>
+                    <span className='header__optionLineOne'>Prime</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
