4bfa16afbb9be60fd5f2935a850041af82e3f102
YS0meone <timyuan1016@gmail.com>
2023-08-30 12:07:02 -0700

    Finished header component and started Home component

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,5 +1,7 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import './Header.css';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 
 function Header() {
     return (
@@ -8,21 +10,27 @@ function Header() {
             <img className='header__logo' src='logo.png' />
             {/* Search Bar */}
             <div className="header__search">
-                <input className='heade__searchInput' type="text" />
+                <input className='header__searchInput' type="text" />
+                <SearchIcon className='header__searchIcon' />
             </div>
+
             {/* Header */}
             <div className='header__nav'>
                 <div className='header__option'>
                     <span className='header__optionLineOne'>Hello Guest</span>
-                    <span className='header__optionLineOne'>Sign In</span>
+                    <span className='header__optionLineTwo'>Sign In</span>
                 </div>
                 <div className='header__option'>
                     <span className='header__optionLineOne'>Returns</span>
-                    <span className='header__optionLineOne'>& Orders</span>
+                    <span className='header__optionLineTwo'>& Orders</span>
                 </div>
                 <div className='header__option'>
                     <span className='header__optionLineOne'>Your</span>
-                    <span className='header__optionLineOne'>Prime</span>
+                    <span className='header__optionLineTwo'>Prime</span>
+                </div>
+                <div className='header__optionBasket'>
+                    <ShoppingBasketIcon />
+                    <span className='header__optionLineTwo header__basketCount'>0</span>
                 </div>
             </div>
         </div>
